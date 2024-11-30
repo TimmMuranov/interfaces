0e23e0650d0d6b8ae0e7a267dab5be0402ce01b7
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-30 22:59:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const windowOuterWidth = window.outerWidth //ширина браузера
 const windowOuterHeight = window.outerHeight //высота браузера
+
+const fileMenue = document.getElementById("fileMenue");
+const openFileMenu = document.getElementById("openFileMenu");
+const tableInside = document.getElementById("tableInside");
+fileMenue.style.display = "none";
+
+openFileMenu.addEventListener("click", function(){
+    if(fileMenue.style.display === "none"){
+        fileMenue.style.display = "block";
+    }
+    else{
+        fileMenue.style.display = "none";
+    }
+})

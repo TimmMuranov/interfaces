2b4f97a49239bd5ad4eeac0b3a337351815a974b
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-12-03 01:49:57 +0300

    Add files via upload
    Добавил кнопки создания файлов.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const tableInside = document.getElementById("tableInside");
 const instruments = document.getElementById("instruments");
 const instrumentsMenue = document.getElementById("instrumentsMenue");
 
+const creatText = document.getElementById("ct");
+const creatDir = document.getElementById("cd");
+const creatImg = document.getElementById("ci");
+
 //===== Обработчики кнопок ========
 function noneBlock(htmlBtn, htmlObj){
     htmlBtn.addEventListener("click", function(){
@@ -18,9 +22,32 @@ function noneBlock(htmlBtn, htmlObj){
     })
 }
 
+function creatFiles(button){
+    let answer = [2];
+    answer[1] = button;
+    button.addEventListener("click", function(){
+        let fileName = prompt("Введите имя");
+        if(fileName === "") {
+            alert("Вы не ввели название.");
+            return;
+        }
+        else if(fileName === null){
+            alert("Отменяем.");
+            return;
+        }
+    answer[2] = fileName;
+    alert("Вы попытались создать файл " + answer[2] + ", нажав на кнопку " + button.id);
+    // дальнейшая обработка нажатия.
+    // массив answer имеет пару "нажатая кнопка - введенное имя"
+    })
+}
+
 fileMenue.style.display = "none";
 instrumentsMenue.style.display = "none";
 
 noneBlock(openFileMenu,fileMenue);
 noneBlock(instruments,instrumentsMenue);
+creatFiles(creatText);
+creatFiles(creatDir);
+creatFiles(creatImg);
 //=====================================
